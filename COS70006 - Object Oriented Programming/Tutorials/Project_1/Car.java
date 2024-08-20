eec22eb1127137e9b00c86c9bfa469f261dfb59d
Arun-2208 <arunragavendhar.1999@gmail.com>
2024-08-21 01:59:26 +1000

    daily update

---
+++ COS70006 - Object Oriented Programming/Tutorials/Project_1/Car.java
@@ -0,0 +1,35 @@
+
+/**
+ * This class has details about the car
+ *
+ * @author - Arun Ragavendhar - 104837257
+ * @version - 1.0 - Project_1
+ */
+class Car
+{
+    // instance variables
+    private String registrationNumber;
+    private String owner;
+    private boolean isStaff;
+    
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car()
+    {
+        System.out.println("Please enter the ");
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
--- COS70006 - Object Oriented Programming/Tutorials/Week4_lab/StudentCreator.java
+++ COS70006 - Object Oriented Programming/Tutorials/Week4_lab/StudentCreator.java
@@ -1,40 +1,24 @@
+
+
 /**
- * The Student class is a very simple representation of a student which
- * contains attributes for the student first and last name and ID number
- * 
- * author : Arun Ragavendhar Arunachalam Palaniyappan - 104837257
- * version : 1.0 - lab 4 -
+ * This program creates at least two student objects with
+ * different values and call getLoginName() to display login name for each student
+ * @author : Arun Ragavendhar Arunachalam Palaniyappan - 104837257
+ * @version : 1.0 - lab 4 -
  */
-
-class Student{
-  private String firstName;
-  private String lastName;
-  private String ID;
-
-/*Constructor for objects of class Student*/
-
-  public Student(String firstName, String lastName, String ID){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.ID = ID;
-  }
-
-/* Returns a string of login name*/
-
-  public String getLoginName (){	 
-    return firstName.substring(0, 1) + lastName;
-	}
-}
 
 public class StudentCreator{
 
   public static void main(String[] args){
 
+// 2 objects have been created for the class Student 
+
     Student s1 = new Student("Arun","Ragavendhar","104837257");
     Student s2 = new Student("Steve","Smith","58292826");
 
+// The output login names for both students is displayed to the user 
+
     System.out.println("Details of the 1st student : " + s1.getLoginName() +'\n');
     System.out.println("Details of the 2nd student : " + s2.getLoginName() +'\n');
-  
   }
 }
