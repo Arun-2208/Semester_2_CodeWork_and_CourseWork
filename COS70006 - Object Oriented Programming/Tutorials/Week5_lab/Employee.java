9d300959c2ee633f516c42491a99b8b4772bfd67
Arun-2208 <arunragavendhar.1999@gmail.com>
2024-08-27 16:08:24 +1000

    daily updates

--- COS70006 - Object Oriented Programming/Tutorials/Week5_lab/Employee.java
+++ COS70006 - Object Oriented Programming/Tutorials/Week5_lab/Employee.java
@@ -1,35 +1,92 @@
-class Employee{
+/**
+ * Employee.java
+ *
+ * This class represents an Employee with a name, employee ID, and salary.
+ * It provides methods to get and set the employee's details.
+ * 
+ * @author Author: Arun Ragavendhar - 104837257
+ * @version - 1.0 - lab 5 
+ */
 
-    String name;
-    int employeeID;
-    double salary;
+ class Employee {
 
-    public Employee(String name, int employeeID, double salary){
+    private String name;          // Employee's name
+    private int employeeID;       // Employee's ID
+    private double salary;        // Employee's salary
+
+    /**
+     * Constructor to initialize the Employee object.
+     * 
+     * @param name The name of the employee.
+     * @param employeeID The unique ID of the employee.
+     * @param salary The salary of the employee.
+     */
+    public Employee(String name, int employeeID, double salary) {
         this.name = name;
         this.employeeID = employeeID;
         this.salary = salary;
     }
-    public String getName(){
+    /**
+     * Gets the name of the employee.
+     * 
+     * @return The name of the employee.
+     */
+    public String getName() {
         return name;
     }
-    public int getEmployeeID(){
+
+    /**
+     * Gets the employee ID.
+     * 
+     * @return The employee ID.
+     */
+    public int getEmployeeID() {
         return employeeID;
     }
-    public double salary(){
+
+    /**
+     * Gets the salary of the employee.
+     * 
+     * @return The salary of the employee.
+     */
+    public double getSalary() {
         return salary;
     }
-    void setName(String name){
+
+    /**
+     * Sets the name of the employee.
+     * 
+     * @param name The new name of the employee.
+     */
+    void setName(String name) {
         this.name = name;
     }
-    void setEmployeeID(int employeeID){
+
+    /**
+     * Sets the employee ID.
+     * 
+     * @param employeeID The new employee ID.
+     */
+    void setEmployeeID(int employeeID) {
         this.employeeID = employeeID;
     }
-    void setSalary(double salary){
+
+    /**
+     * Sets the salary of the employee.
+     * 
+     * @param salary The new salary of the employee.
+     */
+    void setSalary(double salary) {
         this.salary = salary;
     }
 
-    public String toString(){
-        String info = " Name : " + this.name + "Employee ID : " + this.employeeID + "salary : " + this.salary;
+    /**
+     * Returns a string representation of the employee.
+     * 
+     * @return A string containing the employee's name, ID, and salary.
+     */
+    public String toString() {
+        String info = "Name: " + this.name + "  Employee ID: " + this.employeeID + "  Salary: " + this.salary;
         return info;
     }
 }
